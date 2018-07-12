f730676b3c592052ca09daca5f7e6f82e1fae6b3
JBHua <38749478+JBHua@users.noreply.github.com>
2018-07-12 11:10:26 +0800

    StudyDrills33 Modify
    Let user decide where to begin and where to end the while loop.

--- StudyDrills33.1.py
+++ StudyDrills33.1.py
@@ -1,14 +1,30 @@
 #LPTHW Study Drill from Ex33
 
-def while_loop(start_value, end_value, increase):
-    i = start_value
+#Initially I want to let the user decide where to begin and where to end.
+#But I meet a bug. If the user let the starting number be 0, the program will never stop running and go crazy.
+#SO, I gave up on this.
+
+end_number = int(input("Plz give the end number."))
+increasement = int(input("Plz give the increasement."))
+
+def while_loop(end_value, increase):
+    i = 0 
     numbers = []
     while i < end_value:
         print(f"At the top i is {i}")
         numbers.append(i)
 
-        i += 1
+        i += increase
         print("Numbers now: ",numbers)
         print(f"At the bottom i is {i}")
 
-while_loop(0,20,2)
+    return numbers
+
+numbers = while_loop(end_number, increasement)
+
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
